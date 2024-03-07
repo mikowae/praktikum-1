c1cc01fbd54dd5809f24278dc4b1b5b7f05bd30d
firzy <firzy.wijatmiko.ft23@mail.umy.ac.id.com>
2024-03-07 20:13:20 +0700

    Add project files.

---
+++ praktikum 1/praktikum 1.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "======================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
+
+}
+
+void bubbleSortArray() { // procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; // step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (arr[j] > arr[j + 1]) { // step 3
+				int temp = arr[j];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1; //step 4
+
+		cout << endl;
+
+	} while (pass <= n - 1); //step 5
+}
+
+
+void display() {
+	cout << endl;
+	cout << "==================================" << endl;
+	cout << "Elemen Array yang telah tersusun" << endl;
+	cout << "==================================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
+
+
+int main() {
+
+	input(); // Call the input function to get user input
+	bubbleSortArray(); // Call the selection sort function
+	display(); // Display the sorted array
+	system("pause");
+
+	return 0;
+}
